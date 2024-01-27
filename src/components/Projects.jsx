2848ea4866e35d033ae883bfa667abeb6020c313
DanishSofii <bashirdanish85@gmail.com>
2024-01-27 15:48:49 +0530

    commit at 3:48 pm on 27-01-24

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -4,6 +4,7 @@ import img2 from "../images/im2.webp";
 import img3 from "../images/im3.webp";
 import img4 from "../images/img4.webp";
 import img5 from "../images/img5.webp";
+import img6 from "../images/img6.webp";
 import { faGithub } from '@fortawesome/free-brands-svg-icons';
 import { faArrowAltCircleRight } from '@fortawesome/free-solid-svg-icons';
 
@@ -13,14 +14,24 @@ function Projects() {
       id: 1,
       imageSrc: img1,
       title: "Library Management System",
-      description: "LMS built from scratch using EJS, CSS, Express JS , MySql to manage the books in library online.",
+      description: "LMS (FullStack) built from scratch using EJS, CSS, Express JS , MySql to manage the books in library online.",
       links: [
         { text: 'GitHub', url: 'https://github.com/DanishSofii/DBMS-Project', icon: faGithub },
         { text: 'Visit', url: 'https://example.com/link-b',icon: faArrowAltCircleRight }
       ]
     },
-    {   
+    {
       id: 2,
+      imageSrc: img6,
+      title: "Nebula",
+      description: "Blogging website (FullStack) created using React js, Express js, MySql, Tailwind CSS.",
+      links: [
+        { text: 'GitHub', url: 'https://github.com/DanishSofii/Blog-Website', icon: faGithub },
+        { text: 'Visit', url: 'https://blog-website-lemon-zeta.vercel.app/',icon: faArrowAltCircleRight }
+      ]
+    },
+    {   
+      id: 3,
       imageSrc: img2,
       title: "Eat n Split",
       description: " Friends can split easily; Created using React JS  ",
@@ -30,7 +41,7 @@ function Projects() {
       ]
     },
     {
-      id: 3,
+      id: 4,
       imageSrc: img3,
       title: "Weather APP",
       description: "Weather monitoring app using EJS css and JS.",
@@ -40,7 +51,7 @@ function Projects() {
       ]
     },
     {
-      id: 4,
+      id: 5,
       imageSrc: img4,
       title: "TO-DO List Master",
       description: "App for managing daily tasks easily with HTML CSS and JS.",
@@ -50,7 +61,7 @@ function Projects() {
       ]
     },
     {
-      id: 5,
+      id: 6,
       imageSrc: img5,
       title: "Calculator",
       description: "A basic Calculator using HTML, CSS and JS",
