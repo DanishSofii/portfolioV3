c4f174bdcea54f22fcbcb17508f9dc442062a7f4
DanishSofii <bashirdanish85@gmail.com>
2024-03-29 12:13:14 +0530

    resume update

--- src/components/AboutComponent.jsx
+++ src/components/AboutComponent.jsx
@@ -9,7 +9,7 @@ function AboutComponent() {
     setIsMenuOpen(!isMenuOpen);
   };
   const downloadResume =() =>{
-    const resumeFilePath = "https://drive.google.com/file/d/1kS2fvDq9LKORPjvQZr_VNTUcTUg70sQ_/view?usp=sharing";
+    const resumeFilePath = "https://drive.google.com/file/d/1x4zQ62jJEGgvNqzhh9JBc4uINsfz0XJn/view?usp=sharing";
     window.open(resumeFilePath, '_blank'); 
   }
 
